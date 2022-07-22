dc69e0004c86c7e7326368e0a07882d5afdcaa09
Igor Oliveira <m.igoroliveira@gmail.com>
2022-07-22 16:04:24 +0100

    copy btn fixes

--- js/personal-settings.js
+++ js/personal-settings.js
@@ -1,10 +1,74 @@
-function delay(delayInms) {
+const delay = (delayInms) => {
   return new Promise((resolve) => {
     setTimeout(() => {
       resolve(2);
     }, delayInms);
   });
-}
+};
+
+const showCopyPubUrl = async (copyOk) => {
+  const pubUrlCopiedLabel = document.getElementById("pubUrlLabel");
+  pubUrlCopiedLabel.innerText = copyOk ? "Copied" : "Error";
+  pubUrlCopiedLabel.style.display = "unset";
+  await delay(3000);
+  pubUrlCopiedLabel.style.display = "none";
+};
+
+const showCopyEmbUrl = async (copyOk) => {
+  const embedUrlCopiedLabel = document.getElementById("embedUrlLabel");
+  embedUrlCopiedLabel.innerText = copyOk ? "Copied" : "Error";
+  embedUrlCopiedLabel.style.display = "unset";
+  await delay(3000);
+  embedUrlCopiedLabel.style.display = "none";
+};
+
+const copyToClipboard = async (text) => {
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(text).then(
+      () => {
+        return true;
+      },
+      (err) => {
+        alert("copy error: " + err);
+        return false;
+      }
+    );
+  } else {
+    // fallback
+    let textArea = document.createElement("textarea");
+    textArea.value = text;
+
+    // Avoid scrolling to bottom
+    textArea.style.top = "0";
+    textArea.style.left = "0";
+    textArea.style.position = "fixed";
+
+    textArea.style.width = "2em";
+    textArea.style.height = "2em";
+
+    textArea.style.padding = 0;
+
+    textArea.style.border = "none";
+    textArea.style.outline = "none";
+    textArea.style.boxShadow = "none";
+
+    textArea.style.background = "transparent";
+
+    document.body.appendChild(textArea);
+    textArea.focus();
+    textArea.select();
+
+    let copyOK;
+    try {
+      copyOK = document.execCommand("copy");
+    } catch (err) {
+      console.error("copy error:", err);
+      copyOK = false;
+    }
+    document.body.removeChild(textArea);
+    return copyOK;
+  }
+};
 
 document.getElementById("appointments-save-settings").onclick = function () {
   const name = document.getElementById("name");
@@ -54,25 +118,13 @@ document.getElementById("appointments-save-settings").onclick = function () {
   }
 };
 document.getElementById("copyPubUrl").onclick = function () {
-  const cb = navigator.clipboard;
-  const pubUrl = document.getElementById("pubUrl");
-  const pubUrlCopiedLabel = document.getElementById("pubUrlLabel");
-
-  cb.writeText(pubUrl.innerText).then(async () => {
-    pubUrlCopiedLabel.style.display = "unset";
-    await delay(3000);
-    pubUrlCopiedLabel.style.display = "none";
-  });
+  const pubUrl = document.getElementById("pubUrl").innerText;
+  const copyOk = copyToClipboard(pubUrl);
+  showCopyPubUrl(copyOk);
 };
 
 document.getElementById("copyEmbUrl").onclick = function () {
-  const cb = navigator.clipboard;
   const embedUrl = document.getElementById("embedUrl");
-  const embedUrlCopiedLabel = document.getElementById("embedUrlLabel");
-
-  cb.writeText(embedUrl.innerText).then(async () => {
-    embedUrlCopiedLabel.style.display = "unset";
-    await delay(3000);
-    embedUrlCopiedLabel.style.display = "none";
-  });
+  const copyOk = copyToClipboard(embedUrl);
+  showCopyEmbUrl(copyOk);
 };

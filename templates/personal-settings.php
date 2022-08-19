4e4e77bb60388ec8169d6674f276752af374142b
Igor Oliveira <m.igoroliveira@gmail.com>
2022-08-19 12:17:15 +0200

    new settings ui

--- templates/personal-settings.php
+++ templates/personal-settings.php
@@ -9,7 +9,13 @@ style('appointments', 'settings'); // adds a CSS file
 ?>
 
 <div id="appointments" class="section">
-    <h2><?php p($l->t('Appointments')); ?></h2>
+    <div class="header">
+        <h2><?php p($l->t('Appointments')); ?></h2>
+        <div class="save">
+            <div id="saved"><?php p($l->t('Saved')); ?></div>
+            <button id="appointments-save-settings" class="primary"><?php p($l->t("Save Changes")); ?></button>
+        </div>
+    </div>
 
 
     <div class="urls">
@@ -41,24 +47,19 @@ style('appointments', 'settings'); // adds a CSS file
     </div>
     <div class="form-col">
         <label>
-            <?php p($l->t('Name')); ?>
+            <?php p($l->t('Full Name')); ?>
         </label>
         <input type="text" id="name" name="name" value="<?php p($_['name']) ?>" placeholder="<?php p($l->t('Your name')); ?>" autocomplete="on" autocapitalize="none" autocorrect="off" />
+       
+        <label>
+            <?php p($l->t('Phone Number')); ?>
+        </label>
+        <input type="tel" id="phone" name="phone" data-filter='(\+|(\+[1-9])?[0-9]*)' value="<?php p($_['phone']) ?>" placeholder="<?php p($l->t('Your phone number')); ?>" autocomplete="on" autocapitalize="none" autocorrect="off" />
+
         <label>
             <?php p($l->t('E-mail')); ?>
         </label>
         <input type="email" id="email" name="email" value="<?php p($_['email']) ?>" placeholder="<?php p($l->t('Your email address')); ?>" />
-
-        <label>
-            <?php p($l->t('Phone')); ?>
-        </label>
-        <input type="tel" id="phone" name="phone" data-filter='(\+|(\+[1-9])?[0-9]*)' value="<?php p($_['phone']) ?>" placeholder="<?php p($l->t('Your phone number')); ?>" autocomplete="on" autocapitalize="none" autocorrect="off" />
-
-
-    </div>
-    <div>
-        <button id="appointments-save-settings" class="primary"><?php p($l->t("Save")); ?></button>
-        <div id="saved"><?php p($l->t('Saved')); ?></div>
     </div>
 
 </div>

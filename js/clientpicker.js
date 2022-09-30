e9bbbcb13d66e3db960ef4a4b1cc877ef83692e2
Igor Oliveira <m.igoroliveira@gmail.com>
2022-09-30 11:28:18 +0100

    initial serach

--- js/clientpicker.js
+++ js/clientpicker.js
@@ -1,23 +1,38 @@
-// const showClients = (clientNameInput) => {
-//     clientPicker.getElementsByClassName('client-list-item')
-// }
-
 document.addEventListener("DOMContentLoaded", function () {
     const clientPicker = document.getElementById('adminly-client-picker');
     if(clientPicker){
         const clientNameInput = document.getElementById('srgdev-ncfp_fname');
         const clientEmailInput = document.getElementById('srgdev-ncfp_femail');
         const clientPhoneInput = document.getElementById('srgdev-ncfp_fphone');
-        
-        clientNameInput.addEventListener("click", ()=>{clientPicker.style.display = 'block'});
-        // clientNameInput.addEventListener("change", showClients(clientNameInput));
-        clients = clientPicker.getElementsByClassName('client-list-item')
+        let clients = clientPicker.getElementsByClassName('client-list-item');
 
-        clients.forEach(client => client.addEventListener('click', event => {
+        clientNameInput.addEventListener("click", ()=>{clientPicker.style.display = 'block'});
+        clientNameInput.addEventListener("change", ()=>{
+            const searchValue = clientNameInput.value;
+            console.log(searchValue);
+            clients.forEach( client => {
+                const name = client.querySelector('.client-name').innerHTML;
+                if(name.includes(searchValue)){
+                    client.style.display = 'block';
+                }
+                else if(searchValue){
+                    client.style.display = 'none';
+                }
+                else{
+                    client.style.display = 'block';}
+            })
+        });
+
+
+        
+
+        clients.forEach(client => client.addEventListener('click', () => {
             clientNameInput.value = client.querySelector('.client-name').innerHTML;
             clientEmailInput.value = client.querySelector('.client-email').innerHTML;
             client.querySelector('.client-phone') ? clientPhoneInput.value = client.querySelector('.client-phone').innerHTML : clientPhoneInput.value = "";
             clientPicker.style.display = 'none';
         }));
+
+
     }
 });

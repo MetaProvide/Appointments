d6b8709edd2c346f33d25880f11cce08ff0622e5
Igor Oliveira <m.igoroliveira@gmail.com>
2022-10-13 11:47:34 +0100

    Internal Booking Widget Client autofill (#22)

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -20,6 +20,7 @@ use OCP\IUserSession;
 use OCP\Mail\IMailer;
 use OCP\Util;
 use Psr\Log\LoggerInterface;
+use OCA\Adminly_Clients\Db\ClientMapper;
 
 class PageController extends Controller
 {
@@ -35,6 +36,8 @@ class PageController extends Controller
     private $utils;
     private $logger;
     private $userSession;
+    /** @var ClientMapper */
+	private $clientMapper;
 
     public function __construct($AppName,
                                 IRequest $request,
@@ -45,7 +48,8 @@ class PageController extends Controller
                                 IUserSession $userSession,
                                 BackendManager $backendManager,
                                 BackendUtils $utils,
-                                LoggerInterface $logger
+                                LoggerInterface $logger,
+                                ClientMapper $clientMapper
     ) {
         parent::__construct($AppName, $request);
         $this->userId = $UserId;
@@ -57,6 +61,7 @@ class PageController extends Controller
         $this->bc = $backendManager->getConnector();
         $this->utils = $utils;
         $this->logger = $logger;
+        $this->clientMapper = $clientMapper;
     }
 
     /**
@@ -1011,6 +1016,16 @@ class PageController extends Controller
             $ft = $this->l->t('Book Your Appointment');
         }
 
+        $hasActiveValidSession = false;
+        $clientList = [];
+
+        if($this->userSession->getUser()){
+            $hasActiveValidSession = $this->userSession->getUser()->getUid() === $uid;
+            if($hasActiveValidSession){
+                $clientList = $this->clientMapper->findAll($uid);
+            }
+        }
+
         $params = [
             'appt_sel_opts' => '',
             'appt_state' => '0',
@@ -1022,7 +1037,8 @@ class PageController extends Controller
             'appt_gdpr_no_chb' => false,
             'appt_inline_style' => $pps[BackendUtils::PSN_PAGE_STYLE],
             'appt_hide_phone' => $pps[BackendUtils::PSN_HIDE_TEL],
-            'more_html' => ''
+            'more_html' => '',
+            'clients' => $clientList
         ];
 
         // google recaptcha
---
+++ src/clientPicker.js
@@ -0,0 +1,60 @@
+const updateClientList = (clientPicker, clientNameInput, clients) => {
+  clientPicker.style.display = "block";
+  const searchValue = clientNameInput.value.toLowerCase();
+  let results = 0;
+  clients.forEach((client) => {
+    const name = client.querySelector(".client-name").innerHTML.toLowerCase();
+    if (name.includes(searchValue)) {
+      client.style.display = "block";
+      results++;
+    } else if (searchValue) {
+      client.style.display = "none";
+    } else {
+      client.style.display = "block";
+    }
+  });
+  if (searchValue && results === 0) {
+    clientPicker.style.display = "none";     
+    clientNameInput.classList.remove('has-results');
+  } else {
+    clientPicker.style.display = "block";
+    clientNameInput.classList.add('has-results');
+  }
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  const clientPicker = document.getElementById("adminly-client-picker");
+  if (clientPicker) {
+    const clientNameInput = document.getElementById("srgdev-ncfp_fname");
+    const clientEmailInput = document.getElementById("srgdev-ncfp_femail");
+    const clientPhoneInput = document.getElementById("srgdev-ncfp_fphone");
+    let clients = clientPicker.getElementsByClassName("client-list-item");
+
+    clientNameInput.addEventListener("click", (event) => {
+      updateClientList(clientPicker, clientNameInput, clients);
+      event.stopPropagation();
+    });
+    clientNameInput.addEventListener("input", () => {
+      updateClientList(clientPicker, clientNameInput, clients);
+    });
+
+    clients.forEach((client) =>
+      client.addEventListener("click", () => {
+        clientNameInput.value = client.querySelector(".client-name").innerHTML;
+        clientEmailInput.value =
+          client.querySelector(".client-email").innerHTML;
+        client.querySelector(".client-phone")
+          ? (clientPhoneInput.value =
+              client.querySelector(".client-phone").innerHTML)
+          : (clientPhoneInput.value = "");
+        clientPicker.style.display = "none";
+      })
+    );
+
+    window.addEventListener("click", function () {
+      //Hide the menus if visible
+      clientPicker.style.display = "none";
+      clientNameInput.classList.remove('has-results');
+    });
+  }
+});
